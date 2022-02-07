22606b0384dfd95e994c870ee2c4d2bb8b5d1a76
Ewelina179 <ewelinadalek@gmail.com>
2022-02-07 19:25:09 +0100

    added views

--- employee_list/employees/views.py
+++ employee_list/employees/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
 from .models import Employee
+from django.views import View
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 
 def index(request):
     employees = Employee.objects.all()
     return render(request, 'employee/list_of_employees.html', {'employees': employees})
 
+class EmployeeView(DetailView):
+    model = Employee
+    template_name = "employee/employee_detail.html"
+
+class CreateEmployeeView(CreateView):
+    model = Employee
+
+    fields = ['first_name', 'last_name', 'age', 'profession', 'avatar']
+    success_url ="/"
+
+    template_name = "employee/employee_create_form.html"
+    
+class UpdateEmployeeView(UpdateView):
+    model = Employee
+    
+    fields = ['first_name', 'last_name', 'age', 'profession', 'avatar']
+    success_url ="/"
+
+    template_name = "employee/employee_update_form.html"
+
+class DeleteEmployeeView(DeleteView):
+    model = Employee
+    success_url ="/"
+
+    template_name = "employee/employee_delete.html"
