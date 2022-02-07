a7e4d626ab8977bf291eb6373e663d8fe9fa5ccb
Ewelina179 <ewelinadalek@gmail.com>
2022-02-07 23:35:49 +0100

    added settings for development and testing

---
+++ employee_list/employee_list/settings/development.py
@@ -0,0 +1,25 @@
+from .base import *
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'employees',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'postgres',
+
+        'HOST': 'localhost',
+
+        'PORT': '5433',
+
+    }
+
+}
+
+DEBUG = True
