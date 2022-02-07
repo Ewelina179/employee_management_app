d45d1774b1ce76cffccbacfcd2665bb402ffcbbc
Ewelina179 <ewelinadalek@gmail.com>
2022-02-07 19:24:35 +0100

    added urls

--- employee_list/employees/urls.py
+++ employee_list/employees/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 from employee_list.settings import DEBUG, STATIC_URL, STATIC_ROOT, MEDIA_URL, MEDIA_ROOT
 from django.conf.urls.static import static
+from .views import EmployeeView, CreateEmployeeView, UpdateEmployeeView, DeleteEmployeeView
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    #path('upload/', views.upload, name = 'upload-book'),
-    #path('update/<int:book_id>', views.update_book),
-    #path('delete/<int:book_id>', views.delete_book)
+    path('<pk>/details/', EmployeeView.as_view()),
+    path('create/', CreateEmployeeView.as_view()),
+    path('<pk>/edit/', UpdateEmployeeView.as_view()),
+    path('<pk>/delete/', DeleteEmployeeView.as_view()),
 ]
