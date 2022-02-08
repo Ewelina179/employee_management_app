b4f6d524f668e60eebd31694c0434fdff4378da3
Ewelina179 <ewelinadalek@gmail.com>
2022-02-09 00:32:49 +0100

    added tests for EmployeeListView and CreateEmployeeView

--- employee_list/employees/tests/test_views.py
+++ employee_list/employees/tests/test_views.py
@@ -1,1 +1,18 @@
 import pytest
+from pytest_django.asserts import assertTemplateUsed
+from django.urls import reverse
+
+from employees.views import EmployeeListView, CreateEmployeeView
+
+
+def test_employee_list_view(client):
+    response = client.get("/")
+    content = response.content.decode(response.charset)
+    assert response.status_code == 200
+    assert "Lista pracowników" in content
+    assertTemplateUsed(response, 'employee/list_of_employees.html', 'base.html')
+
+def test_create_employee_view(client, db):
+    response = client.post('/create/', data = {"first_name": "Adam", "last_name": "Nowak", "age": "30", "profession": "teacher"})
+    assert response.status_code == 302
+
