03d31a041a6d99d386c54e6f01bc5ac707758605
Ewelina179 <ewelinadalek@gmail.com>
2022-02-08 13:13:30 +0100

    changed model, added choices

--- employee_list/employees/models.py
+++ employee_list/employees/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 class Employee(models.Model):
+    PROFESSION = (
+        ("web developer", "web developer"),
+        ("phtographer", "photographer"),
+        ("data analyst", "data analyst"),
+        ("teacher", "teacher")
+    )
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField(null=True)
-    profession = models.CharField(max_length=50)
+    profession = models.CharField(max_length=50, choices = PROFESSION)
     avatar = models.ImageField(default='default.jpg')
