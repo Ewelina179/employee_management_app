26561d1bf2daad1554b3514a9009672706d84862
Ewelina179 <ewelinadalek@gmail.com>
2022-02-08 08:53:33 +0100

    added form getreportform

--- employee_list/employees/forms.py
+++ employee_list/employees/forms.py
@@ -1,6 +1,23 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Employee
 
 class EmployeeForm(ModelForm):
     class Meta:
         model = Employee
+        fields = ['first_name', 'last_name', 'age', 'profession', 'avatar']
+
+FILE_FORMAT = (
+    ("json", "json"),
+    ("csv", "csv")
+)
+
+unique_profession_of_employees = Employee.objects.distinct("profession").all()
+professions = unique_profession_of_employees.values("profession")
+
+class GetReportForm(forms.Form):
+    # dwa choices - z modelu profession, z wyborów na sztywno format pliku
+    profession = forms.ModelChoiceField(queryset=professions)
+    file_format = forms.ChoiceField(choices = FILE_FORMAT)
+
+
