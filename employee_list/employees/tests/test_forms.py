fbca4300a83d466c160f604c70153c82f13797a3
Ewelina179 <ewelinadalek@gmail.com>
2022-02-09 12:49:26 +0100

    added test for employee form

--- employee_list/employees/tests/test_forms.py
+++ employee_list/employees/tests/test_forms.py
@@ -1,1 +1,16 @@
 import pytest
+
+from employees.models import Employee
+from employees.forms import EmployeeForm
+
+@pytest.mark.django_db
+def test_employee_form_valid():
+    form_data = {
+        "first_name": "test_first_name",
+        "last_name": "test_last_name",
+        "age": "30",
+        "profession": "teacher",
+    }
+    form = EmployeeForm(data=form_data)
+    assert form.is_valid() is True
+
