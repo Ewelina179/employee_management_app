6372f56b61889f69e3958eaff7a69a4ef7ea7f50
Ewelina179 <ewelinadalek@gmail.com>
2022-02-10 17:02:18 +0100

    changed Profession model

--- employee_list/employees/models.py
+++ employee_list/employees/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
 class Employee(models.Model):
-    PROFESSION = (
-        ("web developer", "web developer"),
-        ("phtographer", "photographer"),
-        ("data analyst", "data analyst"),
-        ("teacher", "teacher")
-    )
-
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField(null=True)
-    profession = models.CharField(max_length=50, choices = PROFESSION)
+    profession = models.ForeignKey('employees.Profession', on_delete=models.CASCADE, null=True)
     avatar = models.ImageField(default='default.jpg')
+
+class Profession(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
