fd91c8d676674e5c0dee81a75a54bc272283a059
Ewelina179 <ewelinadalek@gmail.com>
2022-02-10 05:55:33 +0100

    added tests

--- employee_list/employees/tests/test_models.py
+++ employee_list/employees/tests/test_models.py
@@ -2,11 +2,13 @@ import pytest
 
 from employees.models import Employee
 
-@pytest.fixture
-def employee():
-    return Employee.objects.create(first_name="Jan", last_name="Nowak", age="33", profession="teacher")
-
 @pytest.mark.django_db
 def test_employee_in_db(employee):
     employee_in = Employee.objects.get(first_name = "Jan", last_name = "Nowak")
     assert employee == employee_in
+
+@pytest.mark.django_db
+def test_change_employee(employee):
+    employee.name = "Aleksander"
+    employee.save()
+    assert employee.name == "Aleksander"
--- employee_list/employees/tests/test_views.py
+++ employee_list/employees/tests/test_views.py
@@ -1,18 +1,44 @@
 import pytest
 from pytest_django.asserts import assertTemplateUsed
+
 from django.urls import reverse
 
-from employees.views import EmployeeListView, CreateEmployeeView
-
-
-def test_employee_list_view(client):
+def test_employee_list_view(client, db):
     response = client.get("/")
     content = response.content.decode(response.charset)
     assert response.status_code == 200
     assert "Lista pracowników" in content
     assertTemplateUsed(response, 'employee/list_of_employees.html', 'base.html')
 
-def test_create_employee_view(client, db):
+@pytest.mark.django_db
+def test_employee_view(client, employee_first):
+    url_kwargs = {'pk': 1}
+    url = reverse('details',  kwargs=url_kwargs)
+    response = client.get(url)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "employee/employee_detail.html")
+
+@pytest.mark.django_db
+def test_create_employee_view(client):
     response = client.post('/create/', data = {"first_name": "Adam", "last_name": "Nowak", "age": "30", "profession": "teacher"})
     assert response.status_code == 302
+
+@pytest.mark.django_db
+def test_update_employee_view(client, employee_first):
+    url_kwargs = {'pk': 1}
+    url = reverse('update',  kwargs=url_kwargs)
+    response = client.post(url, data = {"first_name": "Ewa"})
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_delete_employee_view(client, employee_first):
+    url_kwargs = {'pk': 1}
+    url = reverse('delete',  kwargs=url_kwargs)
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+
+
+
 
