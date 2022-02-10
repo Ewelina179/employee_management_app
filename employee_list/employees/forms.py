c01c28d2ca41e3e86655c141ffde86da9bf8d3af
Ewelina179 <ewelinadalek@gmail.com>
2022-02-10 17:01:32 +0100

    changed EmployeeForm

--- employee_list/employees/forms.py
+++ employee_list/employees/forms.py
@@ -1,11 +1,16 @@
-from django.forms import ModelForm
+from random import choice
+from django.forms import ModelForm, SelectMultiple
 from django import forms
-from .models import Employee
+from .models import Employee, Profession
 
 class EmployeeForm(ModelForm):
+
+    profession = forms.ModelChoiceField(
+                       widget = forms.Select,
+                       queryset = Profession.objects.all()
+               )
+
     class Meta:
         model = Employee
         fields = ['first_name', 'last_name', 'age', 'profession', 'avatar']
-
-
-
+    
