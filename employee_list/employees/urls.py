2629671dc639d8a95348272d53e04c9d420a4a60
Ewelina179 <ewelinadalek@gmail.com>
2022-02-11 13:13:07 +0100

    added delete_employee_ajax url

--- employee_list/employees/urls.py
+++ employee_list/employees/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from employee_list.settings.base import DEBUG, STATIC_URL, STATIC_ROOT, MEDIA_URL, MEDIA_ROOT
 from django.conf.urls.static import static
-from .views import EmployeeListView, EmployeeView, CreateEmployeeView, UpdateEmployeeView, DeleteEmployeeView, ReportView, DeleteView, CreateProfessionView, UpdateProfessionView, DeleteProfessionView
+from .views import EmployeeListView, EmployeeView, CreateEmployeeView, UpdateEmployeeView, DeleteEmployeeView, ReportView, DeleteEmployeeAjaxView, CreateProfessionView, UpdateProfessionView, DeleteProfessionView
 
 urlpatterns = [
     path('', EmployeeListView.as_view(), name='epmloyee_list_view'),
@@ -11,7 +11,7 @@ urlpatterns = [
     path('<pk>/edit/', UpdateEmployeeView.as_view(), name='update'),
     path('<pk>/delete/', DeleteEmployeeView.as_view(), name='delete'),
     path('report/', ReportView.as_view(), name='url_for_report'),
-    path('delete/', DeleteView.as_view()),
+    path('<pk>/deleteajax/', DeleteEmployeeAjaxView.as_view(), name='delete_employee_ajax'),
     path('csv/',views.getfile, name='report'),
     path('createprofession/', CreateProfessionView.as_view(), name='create_profession'),
     path('<pk>/editprofession/', UpdateProfessionView.as_view(), name='update_profession') ,
