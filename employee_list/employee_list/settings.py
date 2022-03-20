9d1a13221a2e53743e4c1d29903f19c70748f31d
Ewelina179 <ewelinadalek@gmail.com>
2022-03-20 09:56:09 +0100

    changed settings

--- employee_list/employee_list/settings.py
+++ employee_list/employee_list/settings.py
@@ -95,11 +95,11 @@ WSGI_APPLICATION = 'employee_list.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DATABASE_NAME"),
-        'USER': env("DATABASE_USER"),
-        'PASSWORD': env("DATABASE_PASSWORD"),
-        'HOST': env("DATABASE_HOST"),
-        'PORT': env("DATABASE_PORT"),
+        'NAME': os.environ.get("POSTGRES_NAME"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_HOST"),
+        'PORT': os.environ.get("POSTGRES_PORT"),
     }
 }
 AUTH_PASSWORD_VALIDATORS = [
