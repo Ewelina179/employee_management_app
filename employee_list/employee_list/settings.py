0140879f9c8e2dc8bb1bd2a72c684aa7475b5449
Ewelina179 <ewelinadalek@gmail.com>
2022-03-28 10:49:09 +0200

    changed settings

--- employee_list/employee_list/settings.py
+++ employee_list/employee_list/settings.py
@@ -145,11 +145,8 @@ MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-sentry_sdk.init(
-    dsn=os.environ.get('SENTRY_DSN'),
-    integrations=[DjangoIntegration()],
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
+#sentry_sdk.init(
+#    dsn=os.environ.get('SENTRY_DSN'),
+#    integrations=[DjangoIntegration()],
+#
+#)
