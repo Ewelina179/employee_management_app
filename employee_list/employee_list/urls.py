7b394cadc50b149ca47a0b40f6158d9e8c8b5835
Ewelina179 <ewelinadalek@gmail.com>
2022-04-06 12:00:57 +0200

    added urls, changed handlers

--- employee_list/employee_list/urls.py
+++ employee_list/employee_list/urls.py
@@ -1,27 +1,16 @@
-"""employee_list URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
 
-handler400 = 'employees.views.handler400'
-handler404 = 'employees.views.handler404'
-handler500 = 'employees.views.handler500'
+from api.urls import employees_router, profession_router
+
+handler400 = 'api.views.handler400'
+handler404 = 'api.views.handler404'
+handler500 = 'api.views.handler500'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('employees.urls'))
+    path('', include('api.urls')),
+    path('', include(profession_router.urls)),
+    path('', include(employees_router.urls))
 ]
