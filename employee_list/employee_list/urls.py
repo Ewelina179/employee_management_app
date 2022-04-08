d54a18fdf470f29d29bf59c619a2e392f2b7c3f9
Ewelina179 <ewelinadalek@gmail.com>
2022-04-08 16:51:28 +0200

    added i18n_patterns

--- employee_list/employee_list/urls.py
+++ employee_list/employee_list/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from django.conf.urls.i18n import i18n_patterns
 
 from api.urls import employees_router, profession_router
 
@@ -8,9 +9,9 @@ handler400 = 'api.views.handler400'
 handler404 = 'api.views.handler404'
 handler500 = 'api.views.handler500'
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('', include(profession_router.urls)),
     path('', include(employees_router.urls))
-]
+)
