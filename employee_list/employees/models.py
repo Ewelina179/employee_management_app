6bf0e9a211adc9b30b6e039c134775a71d331c76
Ewelina179 <ewelinadalek@gmail.com>
2022-04-20 14:31:36 +0200

    added translation to model

--- employee_list/employees/models.py
+++ employee_list/employees/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class Employee(models.Model):
@@ -6,7 +8,7 @@ class Employee(models.Model):
     last_name = models.CharField(max_length=50)
     age = models.IntegerField(null=True)
     profession = models.ForeignKey('employees.Profession', on_delete=models.PROTECT, null=True)
-    avatar = models.ImageField(default='default.jpg')
+    avatar = models.ImageField(_('Awatar'), help_text=_(' '), default='default.jpg')
 
 
 class Profession(models.Model):
